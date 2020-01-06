bfb2ba5a3c4918642a413aa7d2d9a87a4f5d83ea
Rohit Mittal <mittalrohit7@gmail.com>
2020-01-06 13:28:06 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
+	@GetMapping("/endpointtest")
+	public String eldpointtest()
+	{
+		return "Welcome to endpointtest";
+	}
+	
+	
 	@GetMapping("/test")
 	public String welcome()
 	{
